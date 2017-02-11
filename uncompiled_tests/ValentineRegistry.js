144ab4719168c34d652700f3e5c2f6788cd8c74c
Fabio Berger <me@fabioberger.com>
2017-02-11 13:19:57 +0100

    explicitly name accounts[0] as the "owner" account in tests

--- uncompiled_tests/ValentineRegistry.js
+++ uncompiled_tests/ValentineRegistry.js
@@ -17,12 +17,12 @@ beforeEach(async () => {
 });
 
 contract('ValentineRegistry', accounts => {
-    const accountOne = accounts[0];
+    const owner = accounts[0];
     const accountTwo = accounts[1];
 
         it('should set msg.sender as the contract owner', async () => {
-            const owner = await registry.owner.call({from: accountOne});
-            assert.equal(owner, accountOne, 'owner should be set to the creater of the contract');
+            const owner = await registry.owner.call({from: owner});
+            assert.equal(owner, owner, 'owner should be set to the creater of the contract');
         });
 
         it('should create a targetedValentineRequest request if caller has sufficient gas', async () => {
@@ -30,7 +30,7 @@ contract('ValentineRegistry', accounts => {
             const valentineAddress = accounts[1];
             const result = await registry.createTargetedValentineRequest(requesterName, valentineName, customMessage,
                 valentineAddress, {
-                from: accountOne,
+                from: owner,
                 value: sufficientValue,
             });
             if (!_.includes(_.map(result.logs, log => log.event), 'LogValentineRequestCreated')) {
@@ -47,7 +47,7 @@ contract('ValentineRegistry', accounts => {
                 value: sufficientValue,
             });
 
-            const numRequests = await registry.numRequesters.call({from: accountOne});
+            const numRequests = await registry.numRequesters.call({from: owner});
             const requests = [];
             for (let i = 0; i < numRequests; i++) {
                 let request = await registry.getRequestByIndex.call(i);
@@ -59,13 +59,13 @@ contract('ValentineRegistry', accounts => {
 });
 
 contract('ValentineRegistry', accounts => {
-    const accountOne = accounts[0];
+    const owner = accounts[0];
 
     it('should throw if caller tries to create an openValentineRequest with insufficient ether', async () => {
         const belowRequisiteValue = web3.toWei(0.05, 'ether');
         try {
             await registry.createOpenValentineRequest(requesterName, valentineName, customMessage, {
-                from: accountOne,
+                from: owner,
                 value: belowRequisiteValue,
             });
             assert(false, 'Expected createOpenValentineRequest to throw if insufficient ether paid.');
@@ -80,18 +80,18 @@ contract('ValentineRegistry', accounts => {
 });
 
 contract('ValentineRegistry', accounts => {
-    const accountOne = accounts[0];
+    const owner = accounts[0];
     const accountTwo = accounts[1];
 
     it('should prohibit request updates', async () => {
         const sufficientValue = web3.toWei(0.1, 'ether');
         await registry.createOpenValentineRequest(requesterName, valentineName, customMessage, {
-            from: accountOne,
+            from: owner,
             value: sufficientValue,
         });
         try {
             await registry.createOpenValentineRequest(requesterName, valentineName, customMessage, {
-                from: accountOne,
+                from: owner,
                 value: sufficientValue,
             });
         } catch(err) {
@@ -134,20 +134,21 @@ contract('ValentineRegistry', accounts => {
 });
 
 contract('ValentineRegistry', accounts => {
-    const accountOne = accounts[0];
-    const accountTwo = accounts[1];
+    const owner = accounts[0];
+    const nonOwner = accounts[1];
+    const anotherNonOwner = accounts[2];
 
     it('should cash out entire contract balance when owner calls cashout', async () => {
         const sufficientValue = web3.toWei(0.1, 'ether');
         await registry.createOpenValentineRequest(requesterName, valentineName, customMessage, {
-            from: accountTwo,
+            from: nonOwner,
             value: sufficientValue,
         });
         const balanceBefore = web3.eth.getBalance(registry.address);
         const balanceBeforeInEther = web3.fromWei(balanceBefore, 'ether').toString();
         assert.equal(balanceBeforeInEther, 0.1);
-        const result = await registry.cashout(accountOne, {
-            from: accountOne,
+        const result = await registry.cashout(owner, {
+            from: owner,
         });
         const balanceAfter = web3.eth.getBalance(registry.address);
         const balanceAfterInEther = web3.fromWei(balanceAfter, 'ether').toString();
