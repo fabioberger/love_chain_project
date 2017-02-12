0bb096ba418df8030afcdd267fe51c4b1a0119a7
Fabio Berger <me@fabioberger.com>
2017-02-12 23:37:19 +0100

    Add mainnet truffle configs and differentiate between networks by port number

--- truffle.js
+++ truffle.js
@@ -4,13 +4,19 @@ module.exports = {
     networks: {
         development: {
             host: 'localhost',
-            port: 8546,
+            port: 8545,
             network_id: '*', // Match any network id
         },
         ropsten: {
             host: 'localhost',
-            port: 8545,
+            port: 8546,
             network_id: 3,
+        },
+        mainnet: {
+            host: 'localhost',
+            port: 8547,
+            network_id: 1,
+            gas: 1990000,
         },
     },
     migrations_directory: path.join(__dirname, '/migrations')
