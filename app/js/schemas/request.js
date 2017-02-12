eb9cd06984dd0309540de70b5352193186030f28
Fabio Berger <me@fabioberger.com>
2017-02-12 16:00:08 +0100

    remove non-sensical alphanumeric requirement on names

--- app/js/schemas/request.js
+++ app/js/schemas/request.js
@@ -6,7 +6,7 @@ const MAX_CUSTOM_MESSAGE_LENGTH = 140;
 const MIN_NAME_LENGTH = 1;
 const MAX_NAME_LENGTH = 25;
 
-const nameSpec = Joi.string().alphanum().min(MIN_NAME_LENGTH).max(MAX_NAME_LENGTH).required();
+const nameSpec = Joi.string().min(MIN_NAME_LENGTH).max(MAX_NAME_LENGTH).required();
 const addressSpec = Joi.string().alphanum().length(42, 'utf8').regex(/^(0x)[0-9A-Za-z]{40}/);
 
 const RequestSchema = Joi.object().keys({
